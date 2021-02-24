00a0b79f41b1d3fb019d8937feedd97ef2a71c9c
HoopDead <polec0@wp.pl>
2021-02-24 11:44:17 +0100

    Documentation for Window & Game header files

--- Game.h
+++ Game.h
@@ -6,19 +6,43 @@ class Game {
 
 public:
 
-	//TODO: Documentation
+	/* Constructor of Game Object
+	* Intialise code for Game class, right now it sets Window object running
+	* @Param: none
+	* @Return: none
+	*/
 	Game();
 
-	//TODO: Documentation
+	/*
+	* Update method
+	* Handling the most of game logic - movement, AI & animation updates. Right now it updates only window class with update method
+	* @Param: none
+	* @Return: none
+	*/
 	void update();
 
-	//TODO: Documentation
+	/*
+	* LateUpdate method
+	* Handling the logic relies on data calculated in update method.
+	* @Param: none
+	* @Return: none
+	*/
 	void lateUpdate();
 
-	//TODO: Documentation
+	/*
+	* Draw method
+	* When every Entity object is updated it needs to be drawed on screen. Method is responsible for drawing everything.
+	* @Param: none
+	* @Return: none
+	*/
 	void draw();
 
-	//TODO: Documentation
+	/*
+	* isRunning method
+	* Returns whether window is alive, or dead
+	* @Param: none
+	* @Return: boolean represents the state logic state of window.
+	*/
 	bool isRunning() const;
 
 private:
--- Window.h
+++ Window.h
@@ -7,22 +7,52 @@ class Window {
 
 public:
 
-	//TODO: Documentation
+	/*
+	* Construtor of Window object
+	* Method creates the new window and sets every option of it provided by user
+	* @Param: const std::string reference to Window Name - programmer can provide the name of window
+	* @Return: -
+	*/
 	Window(const std::string& windowName);
 
-	//TODO: Documentation
+	/*
+	* Update method
+	* Listen for event provided by player from keyboard
+	* @Param: -
+	* @Return: -
+	*/
 	void update();
 
-	//TODO: Documentation
+	/*
+	* beginDraw method
+	* Called, on begin of render method - it has to clear the screen, because otherwise we would see all previous framerates
+	* @Param: -
+	* @Return: -
+	*/
 	void beginDraw();
 
-	//TODO: Documentation
+	/*
+	* draw method
+	* Called to draw everything on screen
+	* @Param: const sf::Drawable (SFML virtual object) reference to drawable - you can pass any drawable object by it. (Rectangle, Sprite etc.)
+	* @Return: -
+	*/
 	void draw(const sf::Drawable& drawable);
 
-	//TODO: Documentation
+	/*
+	* endDraw method
+	* Called, to display window on screen
+	* @Param: -
+	* @Return: -
+	*/
 	void endDraw();
 
-	//TODO: Documentation
+	/*
+	* isOpen method
+	* Returns whether window is alive, or dead
+	* @Param: none
+	* @Return: boolean represents the state logic state of window.
+	*/
 	bool isOpen() const;
 
 private:
