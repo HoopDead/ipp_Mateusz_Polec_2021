f9e8757d06b24763a0aa3fb7a35e446250ccd0c1
HoopDead <polec0@wp.pl>
2021-05-04 16:15:39 +0200

    Deleted Graphics from remote repo

--- Graphics/map/Dark Tokyo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Dark Tokyo" tilewidth="32" tileheight="32" tilecount="1050" columns="42">
- <image source="Dark Tokyo.png" trans="6f6f6f" width="1344" height="800"/>
-</tileset>
