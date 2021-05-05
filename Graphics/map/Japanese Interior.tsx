e360d0958a717d6ca2a898056f26da1678c9f00c
HoopDead <polec0@wp.pl>
2021-05-05 15:16:41 +0200

    Deleted graphics

--- Graphics/map/Japanese Interior.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Japanese Interior" tilewidth="32" tileheight="32" tilecount="1365" columns="65">
- <image source="Japanese Interior.png" trans="6f6f6f" width="2080" height="672"/>
-</tileset>
